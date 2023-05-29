39126ffda8f64088f2a9ca867d9fc508b02e7cdb
doroz <125635258+devdoroz@users.noreply.github.com>
2023-05-28 18:55:30 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -302,13 +302,13 @@ local window = {}; do
 		local notifHideTween = tweenService:Create(notificationClone, tInfo, {Position = UDim2.new(1.2, 0, notificationClone.Position.Y.Scale, 0)})
 		local function upNotifs()
 			for index, notification in pairs(self.UI.Notifications:GetChildren()) do
-				local t = tweenService:Create(notification, tInfo, {Position = notification.Position + UDim2.new(0, 0, 0.17, 0)})
+				local t = tweenService:Create(notification, tInfo, {Position = notification.Position - UDim2.new(0, 0, 0.17, 0)})
 				t:Play()
 			end
 		end
 		local function downNotifs()
 			for index, notification in pairs(self.UI.Notifications:GetChildren()) do
-				local t = tweenService:Create(notification, tInfo, {Position = notification.Position - UDim2.new(0, 0, 0.17, 0)})
+				local t = tweenService:Create(notification, tInfo, {Position = notification.Position + UDim2.new(0, 0, 0.17, 0)})
 				t:Play()
 			end
 		end
